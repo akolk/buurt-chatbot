0cb23f5239d31cca39180f5de031931e344d12e2
Anjo Kolk <anjo.kolk@gmail.com>
2024-08-16 08:42:29 +0200

    Create card_text.py

---
+++ components/card_text.py
@@ -0,0 +1,15 @@
+import    as dbc
+
+def makecard(cardtitle,title,body):
+    return dbc.Card(
+       [
+        dbc.CardHeader(cardtitle),
+        dbc.CardBody(
+            [
+                html.H5(title, className="card-title"),
+                html.P(body, className="card-text"),
+            ]
+        ),
+       ],
+       className="shadow-lg p-3 mb-5 bg-white rounded"
+    )
