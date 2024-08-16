99e6662d1fa43463d2ff59ce65e85a27a1a32ac8
Anjo Kolk <anjo.kolk@gmail.com>
2024-08-16 08:52:56 +0200

    Update graphql.py

--- services/graphql.py
+++ services/graphql.py
@@ -1,1 +1,8 @@
 
+
+def graphql_endpoint(query):
+    try:
+        response = requests.post(GRAPHQL_ENDPOINT, json={"query": query})
+        return response.json()
+    except Exception as e:
+        return {'query': 'Sorry, there was an error contacting the graphql server.'}
