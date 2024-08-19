9f837be83b238377d27058c0a8b6fe9ebad92e40
Anjo Kolk <anjo.kolk@gmail.com>
2024-08-19 10:59:57 +0200

    Update navbar.py

--- components/navbar.py
+++ components/navbar.py
@@ -2,6 +2,11 @@ import dash_bootstrap_components as dbc
 
 def render_navbar(brand_name:str = "Chatbot", brand_color:str = "#165AA7"):
     navbar = dbc.NavbarSimple(
+            children=[
+                dbc.NavItem(dbc.NavLink("Home", href="#")),
+                dbc.NavItem(dbc.NavLink("Over", href="#")),
+                dbc.Button("Reset", id="reset-button", color="danger", className="ml-auto")
+                ],
             brand=brand_name,
             brand_href="/",
             color=brand_color,
