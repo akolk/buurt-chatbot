c5767b37d5ee7ab7bcdfba4b6fede8922e6b62af
Anjo Kolk <anjo.kolk@gmail.com>
2024-08-22 21:30:23 +0200

    Create map.py

---
+++ components/map.py
@@ -0,0 +1,5 @@
+import dash_leaflet as dl
+import dash_leaflet.express as dlx
+
+def mapbrt(location):
+    return dl.Map(center=location, zoom=8, children=[dl.TileLayer(url='https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0/standaard/EPSG:3857/{z}/{x}/{y}.png')])
