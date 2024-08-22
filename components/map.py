877e8cd3f6b2fc57c5407c427238481c3109df6f
Anjo Kolk <anjo.kolk@gmail.com>
2024-08-22 22:16:51 +0200

    Update map.py

--- components/map.py
+++ components/map.py
@@ -2,4 +2,8 @@ import dash_leaflet as dl
 import dash_leaflet.express as dlx
 
 def mapbrt(location):
-    return dl.Map(center=location, zoom=8, children=[dl.TileLayer(url='https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0/standaard/EPSG:3857/{z}/{x}/{y}.png')])
+    return dl.Map(center=location, 
+                  zoom=8, 
+                  children=[dl.TileLayer(url='https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0/standaard/EPSG:3857/{z}/{x}/{y}.png')],
+                  style={'width': '100%', 'height': '50vh'}
+                 )
