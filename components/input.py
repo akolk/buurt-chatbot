1435c67debc620ed2a1fee041e47fd8b1fc74d7b
Anjo Kolk <anjo.kolk@gmail.com>
2024-08-23 21:52:01 +0200

    fix de input

--- components/input.py
+++ components/input.py
@@ -3,18 +3,29 @@ from dash import dcc, html
 
 def render_chat_input():
     chat_input = dbc.InputGroup(
-        children=[
-            dbc.Input(id="user-input", placeholder="Stel een vraag ....", type="text"),
+    children=[
+        dbc.Input(id="user-input", placeholder="Stel een vraag ....", type="text"),
+        html.Div(
             dcc.Upload(
                 id='upload-data',
                 children=html.Div([
-                  'Sleep en plaats of ',
-                   html.A('selecteer bestand(en)')
+                    'Sleep, plaats of ',
+                    html.A('selecteer bestand(en)')
                 ]),
-                multiple=True  # Allow multiple files to be uploaded
+                multiple=True,  # Allow multiple files to be uploaded
+                style={
+                    'width': '100%',
+                    'height': '100%',
+                    'lineHeight': '38px',
+                    'borderWidth': '1px',
+                    'borderStyle': 'dashed',
+                    'borderRadius': '4px',
+                    'textAlign': 'center',
+                }
             ),
-            dbc.Button(id="submit", children=">", color="success"),
-            
-        ],
+            className="form-control",  # Ensure it behaves like an input field
+        ),
+        dbc.Button(id="submit", children=">", color="success"),
+    ],
     )
     return chat_input
