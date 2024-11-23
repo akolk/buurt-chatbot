736c4e7041a6b38723adcf915f7c4e577cc6e2b8
Anjo Kolk <anjo.kolk@gmail.com>
2024-11-24 00:12:57 +0100

    Update card_text.py

--- components/card_text.py
+++ components/card_text.py
@@ -4,10 +4,10 @@ from dash import dcc, html
 def makecard(cardtitle,title,body, style, className):
     return dbc.Card(
        [
-        dbc.CardHeader(cardtitle),
+        #dbc.CardHeader(cardtitle),
         dbc.CardBody(
             [
-                html.H5(title, className="card-title"),
+                html.H5(cardtitle, className="card-title"),
                 html.P(body, className="card-text"),
             ]
         ),
