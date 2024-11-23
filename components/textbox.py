a687855c5d2ddf4e8402d99d683e82cafd155f76
Anjo Kolk <anjo.kolk@gmail.com>
2024-11-23 21:01:24 +0100

    Update textbox.py

--- components/textbox.py
+++ components/textbox.py
@@ -32,7 +32,7 @@ def render_textbox(obj, box:str = "AI"):
         )
         #textbox_human = dbc.Card(obj['question'], style=style, body=True, color="primary", inverse=True
         #,                 className="shadow-lg p-3 mb-5 bg-black rounded"
-        textbox_human = dbc.Card(obj['question'], style=style, body=True, inverse=True, className=""sticky-note-card")
+        textbox_human = dbc.Card(obj['question'], style=style, body=True, inverse=True, className="sticky-note-card")
         return html.Div([thumbnail_human, textbox_human])
 
     elif box == "AI":
